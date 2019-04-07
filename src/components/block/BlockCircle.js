710b4b8466c50bf2270d590fa924df6eb98fa0ba
stefano.bassan <theobassan@gmail.com>
2019-04-07 14:03:51 -0300

    map bug

--- src/components/block/BlockCircle.js
+++ src/components/block/BlockCircle.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import reactCSS from 'reactcss'
+import map from 'lodash/map'
 import merge from 'lodash/merge'
 import color from '../../helpers/color'
 
